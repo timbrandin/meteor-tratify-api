0f9ce6ede0503a91d43feb3ac0b8d918460cb077
Tim Brandin <tim.brandin@gmail.com>
2015-06-03 19:46:46 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'traitify:api',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Traitify API bundled for Meteor',
   // URL to the Git repository containing the source code for this package.
